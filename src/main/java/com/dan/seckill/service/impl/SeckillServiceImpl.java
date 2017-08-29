be72e339f5920408bce4161c5c2929cf23a2ae0a
lingd3 <824928207@qq.com>
2017-08-29 20:06:33 +0800

    service层全部实现

--- src/main/java/com/dan/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/com/dan/seckill/service/impl/SeckillServiceImpl.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import com.dan.seckill.dao.SeckillDao;
@@ -66,6 +67,13 @@ public class SeckillServiceImpl implements SeckillService {
 		return md5;
 	}
 
+	@Transactional
+	/**
+	 * 使用注解控制事务方法的优点：
+	 * 1：开发团队达成一致约定，明确标注事务方法的编程风格
+	 * 2：保证事务方法的执行时间尽可能短，不要穿插其他网络操作RPC/HTTP请求或者剥离到事务方法外部
+	 * 3：不是所有的方法都需要事务，如只有一条修改操作，只读操作不需要事务控制
+	 */
 	public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5)
 			throws SeckillException, RepeatKillException, SeckillCloseException {
 		if (md5 == null || !md5.equals(getMD5(seckillId))) {
